d5dfdf8ce98e8c6d0f52ff92d39433892e92b939
SHAHZAIB <shahzaibkhanmsk1@gmail.com>
2026-06-18 17:18:13 +0500

    80% complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,6 +89,8 @@ if (searchBtn && searchInput) {
 }
 
 // sent to email
+const btn = document.getElementById("btn");
+const toast = document.getElementById("toast");
 
 if (typeof emailjs !== "undefined") {
     emailjs.init("UJb-VdvnbrbEsOb59");
@@ -154,48 +156,181 @@ if (form) {
 
 
 
-let forms = document.querySelectorAll(".form");
+// let forms = document.querySelectorAll(".form");
+// forms.forEach(function(form) {
+//   form.addEventListener("submit", function(e) {
+//     e.preventDefault();
 
-forms.forEach(function(form) {
-  form.addEventListener("submit", function(e) {
+//     let inputs = form.querySelectorAll(".fields");
+//     let isValid = true;
+//     inputs.forEach(function(input) {
+//       if(input.value.trim() === "") {
+//         isValid = false;
+//         input.style.borderBottom = "2px solid red";
+//       }else{
+//         input.style.borderBottom = "2px solid var(--first-color)";
+//       }
+//     });
+// let pass = document.getElementById("pass");
+//     let cpass = document.getElementById("cpass");
+// if(pass && cpass){
+// let passValue = pass.value.trim();
+// let cpassValue = cpass.value.trim();
+//     if(passValue !== cpassValue){
+//         pass.style.borderBottom = "2px solid red";
+//         cpass.style.borderBottom = "2px solid red";
+
+//               toast.innerHTML = "Passwords do not match ❌";
+//       toast.classList.add("show"); 
+//       isValid = false;
+
+//       setTimeout(() => {
+//     toast.classList.remove("show");
+// }, 5000);
+// return;
+//     }else{
+//         pass.style.borderBottom = "2px solid var(--first-color)";
+//         cpass.style.borderBottom = "2px solid var(--first-color)";
+        
+//     }
+//       };
+  
+//     (isValid) {
+//         toast.innerHTML = "Form submitted successfully ✅";
+//       toast.classList.add("show");
+//       setTimeout(() => {
+//     toast.classList.remove("show");
+// }, 5000);
+
+//       form.reset();
+//     } else {
+//         toast.innerHTML = "Please fill all fields ❌";
+//         toast.classList.add("show");
+//       setTimeout(() => {
+//     toast.classList.reifmove("show");
+// }, 5000);
+//     }
+//   });
+// });
+
+
+let signupForms = document.querySelector(".signup-form");
+if(signupForms) {
+signupForms.addEventListener("submit", function(e) {
     e.preventDefault();
-
-    let inputs = form.querySelectorAll(".fields");
+    let inputs = signupForms.querySelectorAll(".fields-signup");
     let isValid = true;
-
     inputs.forEach(function(input) {
       if(input.value.trim() === "") {
         isValid = false;
-        input.style.border = "2px solid red";
-      } else {
-        input.style.border = "2px solid gray";
+        input.style.borderBottom = "2px solid red";
+      }else{
+        input.style.borderBottom = "2px solid var(--first-color)";
       }
     });
+let pass = document.getElementById("pass");
+    let cpass = document.getElementById("cpass");
 
-    if(isValid) {
-        toast.innerHTML = "Form submitted successfully ✅";
-      toast.classList.add("show");
-      setTimeout(() => {
+    if(pass.value.trim() === ""){
+    pass.style.borderBottom = "2px solid red";
+    isValid = false;
+}
+
+if(cpass.value.trim() === ""){
+    cpass.style.borderBottom = "2px solid red";
+    isValid = false;
+}
+
+if(pass.value.trim() !== "" && cpass.value.trim() !== "" && pass.value.trim() !== cpass.value.trim()){
+    toast.innerHTML = "Passwords do not match ❌";
+      toast.classList.add("show"); 
+      isValid = false;  
+        setTimeout(() => {
     toast.classList.remove("show");
-}, 5000);
+}, 3000);
+        pass.style.borderBottom = "2px solid red";
+        cpass.style.borderBottom = "2px solid red";
+        return;
+}
 
-      form.reset();
-    } else {
+    if(isValid){
+    const user = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("pass").value,
+        confirmPassword: document.getElementById("cpass").value
+    };
+    console.log(user);
+    localStorage.setItem("user", JSON.stringify(user));
+    toast.innerHTML = "Account Created Successfully ✅";
+    toast.classList.add("show");
+     setTimeout(() => {
+    toast.classList.remove("show");
+}, 3000);
+
+    signupForms.reset();
+}else {
         toast.innerHTML = "Please fill all fields ❌";
         toast.classList.add("show");
       setTimeout(() => {
     toast.classList.remove("show");
-}, 5000);
-    }
-  });
+}, 3000);
+    
+}
 });
+}
 
-const btn = document.getElementById("btn");
-const toast = document.getElementById("toast");
+// login form
 
-btn.addEventListener("click", () => {
+const loginForm = document.querySelector(".form");
+if(loginForm) {
+loginForm.addEventListener("submit", function(e){
 
+    e.preventDefault();
+
+    let Lemail = document.getElementById("Lemail");
+    let Lpass = document.getElementById("Lpass");
+
+ if(Lemail.value.trim() === "" || Lpass.value.trim() === ""){
+    if(Lemail.value.trim() === ""){
+        Lemail.style.borderBottom = "2px solid red";
+    }
+    if(Lpass.value.trim() === ""){
+        Lpass.style.borderBottom = "2px solid red";
+    }
+    toast.innerHTML = "Please fill all fields ❌";
     toast.classList.add("show");
 
+    setTimeout(() => {
+        toast.classList.remove("show");
+    }, 3000);
+
+    return;
+}
+    const email = document.getElementById("Lemail").value;
+    const password = document.getElementById("Lpass").value;
+
+    const storedUser =
+        JSON.parse(localStorage.getItem("user"));
+
+    if(
+        storedUser &&
+        storedUser.email === email &&
+        storedUser.password === password
+    ){
+        window.open("./login.html", "_blank");
+    }
+    else{
+        toast.innerHTML = "Invalid email or password ❌";
+      toast.classList.add("show");  
+        setTimeout(() => {
+    toast.classList.remove("show");
+}
+, 3000);
+     Lemail.style.borderBottom = "2px solid red";
+     Lpass.style.borderBottom = "2px solid red";
     
+    }
+
 });
+}
